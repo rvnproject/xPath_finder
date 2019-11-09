4fb06076f44de1dc1d375210c7808f3c75bfcb92
Paul <paul.brochard96@gmail.com>
2019-11-09 18:24:17 +0100

    add libgen isbn validation

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ isbn_request = requests.post(
 	})
 
 
-tree = html.fromstring(isbn_request.content)
-isbn_list = list(map(lambda x: x[0:-1], tree.xpath("*//table[contains(@class, 'datatable')]//tbody//tr//td[2]//text()")))
 
+wokinfo_tree = html.fromstring(isbn_request.content)
+isbn_list = list(map(lambda x: x[0:-1], wokinfo_tree.xpath("*//table[contains(@class, 'datatable')]//tbody//tr//td[2]//text()")))
+
+libgen_request = requests.post(
+	"http://libgen.lc/batchsearchindex.php?lgtopic=libgen", 
+	data={"Content-Disposition": "form-data", "dsk": "\n".join(isbn_list[0:4900]), "isbn": "1"}
+)
+
+libgen_tree = html.fromstring(libgen_request.content)
+libgen_missing_isbn_list = list(libgen_tree.xpath("//table[last()]/tr[ td[3]/text() = 0]/td[1]//text()"))
